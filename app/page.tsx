034f55701bae420d30f3a7f8e885c40aa9fe33f9
Bridger Tower <bridgertower@gmail.com>
2024-09-20 14:30:39 -0600

    Updated

--- app/page.tsx
+++ app/page.tsx
@@ -3,21 +3,16 @@ import { AnimatedText } from "./animated-text";
 
 export default function Home() {
   return (
-    <main className="p-6 md:p-12">
-      <div className="grid max-w-2xl">
-        <Item>
-          <h1>
-            <AnimatedText text="WIP Design"></AnimatedText>
-          </h1>
-          <p>
-            <AnimatedText
-              text="A collection of design tools and resources."
-              delay={0.5}
-            ></AnimatedText>
-          </p>
-        </Item>
-        <div></div>
-      </div>
+    <main>
+      <h1>
+        <AnimatedText text="WIP Design"></AnimatedText>
+      </h1>
+      <p>
+        <AnimatedText
+          text="A collection of design tools and resources."
+          delay={0.5}
+        ></AnimatedText>
+      </p>
     </main>
   );
 }
