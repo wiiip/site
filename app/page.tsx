13d59958fd97691ad113aa26e61769bcbcf82e87
Bridger Tower <bridgertower@gmail.com>
2024-09-20 13:51:33 -0600

    Updated

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,16 @@
+import { cn } from "@/lib/utils";
+
 export default function Home() {
   return (
-    <main className="font-mono p-6 md:p-12 border min-h-screen">
+    <main
+      className={cn(
+        "font-mono min-h-screen",
+        "p-6 md:p-12",
+        "border",
+        "grid grid-cols-4",
+        "divide-x divide-y",
+      )}
+    >
       <h1>WIP Design</h1>
     </main>
   );
