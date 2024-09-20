1d0ed57665053175644d15c0573fad9354aaf8fe
Bridger Tower <bridgertower@gmail.com>
2024-09-20 15:27:21 -0600

    Updated

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,47 @@
 import React from "react";
 import Link from "next/link";
+import Image from "next/image";
+import Logo from "@/public/logo.svg";
 
 export default function Home() {
   return (
     <Main>
       <Animate>
+        <Image
+          src={Logo}
+          alt="WIP Design Logo"
+          width={64}
+          height={35.8}
+          className="-ml-1"
+        />
+        <div></div>
         <h1>WIP Design, tools and resources for designers.</h1>
         <h2 className="text-muted">
           <em>Work in Progress</em> is a mindset. It is a way of living. Never
           stop learning. Never stop growing. Never stop becoming. A designer is
-          constantly on that path. At <a href="https://wip.ac">WIP</a> I wanted
-          to build tools for the WIP Designer. This is a collection of those
-          tools and resources created by your fellow designer,{" "}
-          <a href="https://bridger.to/x">Bridger Tower</a>.
+          constantly on that path. At{" "}
+          <a className="inline-link" target="_blank" href="https://wip.ac">
+            WIP
+          </a>{" "}
+          I wanted to build tools for the WIP Designer. This is a collection of
+          those tools and resources created by your fellow designer,{" "}
+          <a
+            className="inline-link"
+            target="_blank"
+            href="https://bridger.to/x"
+          >
+            Bridger Tower
+          </a>
+          .
         </h2>
         <div></div>
         <h3>Tools</h3>
-        <Item href="/test">Example</Item>
+        <Item href="/test">Example Tool</Item>
         <div></div>
         <h3>Resources</h3>
+        <Item href="https://bridger.to" inProgress>
+          Example Resource
+        </Item>
       </Animate>
     </Main>
   );
@@ -50,12 +73,17 @@ function Animate({ children }: { children: React.ReactNode }) {
 
 function Item({
   children,
-  inProgress = false,
+  inProgress = true,
   href,
 }: {
   children: React.ReactNode;
-  inProgress: Boolean;
-  href: URL;
+  inProgress?: Boolean;
+  href: string;
 }) {
-  return <Link href={href}>{children}</Link>;
+  return (
+    <Link href={href}>
+      {children}
+      {inProgress && <span className="text-muted ml-2">(In Progress)</span>}
+    </Link>
+  );
 }
