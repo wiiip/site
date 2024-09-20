cb15cc3ebf0be2a5a91031502ba8bdfa8a98bdaa
Bridger Tower <bridgertower@gmail.com>
2024-09-20 14:07:08 -0600

    Updated

--- app/page.tsx
+++ app/page.tsx
@@ -2,16 +2,51 @@ import { cn } from "@/lib/utils";
 
 export default function Home() {
   return (
-    <main
-      className={cn(
-        "font-mono min-h-screen",
-        "p-6 md:p-12",
-        "border",
-        "grid grid-cols-4",
-        "divide-x divide-y"
-      )}
-    >
-      <h1>WIP Design</h1>
+    <main className="p-6 md:p-12">
+      <Grid>
+        <Item cols={4} rows={1}>
+          <h1 className="text-2xl font-bold">WIP Design</h1>
+        </Item>
+        <Item cols={4} rows={1}>
+          <p className="text-lg">
+            A collection of design tools and resources created by{" "}
+            <a
+              href="https://bridger.to"
+              className="underline hover:text-blue-600 transition-colors"
+            >
+              Bridger Tower.
+            </a>
+          </p>
+        </Item>
+      </Grid>
     </main>
   );
 }
+
+const Grid = ({ children, cols = 8, rows = 8 }: GridProps) => {
+  return (
+    <div
+      className={cn(
+        "font-mono min-h-screen",
+        `grid grid-cols-${cols} grid-rows-${rows}`,
+        "max-w-screen-5xl mx-auto"
+      )}
+    >
+      {children}
+    </div>
+  );
+};
+
+const Item = ({ children, cols = 1, rows = 1 }: ItemProps) => {
+  return (
+    <div
+      className={cn("p-4 md:p-6", "border")}
+      style={{
+        gridColumn: `span ${cols} / span ${cols}`,
+        gridRow: `span ${rows} / span ${rows}`,
+      }}
+    >
+      {children}
+    </div>
+  );
+};
---
+++ types.ts
@@ -0,0 +1,11 @@
+interface GridProps {
+  children: React.ReactNode;
+  cols?: number;
+  rows?: number;
+}
+
+interface ItemProps {
+  children: React.ReactNode;
+  cols?: number;
+  rows?: number;
+}
