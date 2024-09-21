bd75e990e285f8e0757881ae3bb589683b93a86b
Bridger Tower <bridgertower@gmail.com>
2024-09-20 21:19:38 -0600

    Updated

--- app/email-form.tsx
+++ app/email-form.tsx
@@ -5,8 +5,9 @@
 import { useState } from "react";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
-import { motion, AnimatePresence } from "framer-motion";
+import { motion, AnimatePresence, MotionConfig } from "framer-motion";
 import * as z from "zod";
+import useMeasure from "react-use-measure";
 
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
@@ -27,9 +28,12 @@ const formSchema = z.object({
   }),
 });
 
+const transition = { type: "ease", ease: "easeInOut", duration: 0.4 };
+
 export function EmailForm({ label }: { label?: string }) {
   const [isSubmitted, setIsSubmitted] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
+  const [ref, bounds] = useMeasure();
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -71,48 +75,71 @@ export function EmailForm({ label }: { label?: string }) {
   }
 
   return (
-    <AnimatePresence mode="wait">
-      {!isSubmitted ? (
-        <motion.div
-          key="form"
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          exit={{ opacity: 0 }}
-        >
-          <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-2">
-              <FormField
-                control={form.control}
-                name="email"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>{label}</FormLabel>
-                    <FormControl>
-                      <Input placeholder="dieter.rams@gmail.com" {...field} />
-                    </FormControl>
-                    <FormDescription>
-                      Enter your email address to subscribe.
-                    </FormDescription>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <Button type="submit" disabled={isLoading}>
-                {isLoading ? "Subscribing..." : "Subscribe"}
-              </Button>
-            </form>
-          </Form>
-        </motion.div>
-      ) : (
-        <motion.div
-          key="thank-you"
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ duration: 0.5 }}
-        >
-          Thank you for subscribing.
-        </motion.div>
-      )}
-    </AnimatePresence>
+    <MotionConfig transition={transition}>
+      <motion.div
+        animate={{ height: bounds.height > 0 ? bounds.height : undefined }}
+        transition={{ type: "spring", bounce: 0.2, duration: 0.8 }}
+      >
+        <div ref={ref}>
+          <AnimatePresence mode="wait">
+            {!isSubmitted ? (
+              <motion.div
+                key="form"
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                exit={{ opacity: 0 }}
+                transition={{
+                  ...transition,
+                  duration: transition.duration / 2,
+                }}
+              >
+                <Form {...form}>
+                  <form
+                    onSubmit={form.handleSubmit(onSubmit)}
+                    className="space-y-2"
+                  >
+                    <FormField
+                      control={form.control}
+                      name="email"
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormLabel>{label}</FormLabel>
+                          <FormControl>
+                            <Input
+                              placeholder="dieter.rams@gmail.com"
+                              {...field}
+                            />
+                          </FormControl>
+                          <FormDescription>
+                            Enter your email address to subscribe.
+                          </FormDescription>
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
+                    <Button type="submit" disabled={isLoading}>
+                      {isLoading ? "Subscribing..." : "Subscribe"}
+                    </Button>
+                  </form>
+                </Form>
+              </motion.div>
+            ) : (
+              <motion.div
+                key="thank-you"
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{
+                  ...transition,
+                  duration: transition.duration / 2,
+                  delay: transition.duration / 2,
+                }}
+              >
+                <p className="text-sm">Thank you for subscribing.</p>
+              </motion.div>
+            )}
+          </AnimatePresence>
+        </div>
+      </motion.div>
+    </MotionConfig>
   );
 }
